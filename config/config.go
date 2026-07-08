26fed02f756c0f6882e1bc722207ce4537f9bfc5
smilecat666 <smilecat666@proton.me>
2026-07-08 20:52:04 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,141 +1,84 @@
 // [hellcat]
+
 package config
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-	"time"
-
-	"hellcat/parser"
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+    "hellcat/parser"
 )
-
-type LogConfig struct {
-	LogLevel string `json:"loglevel"`
-}
-
 type XrayConfig struct {
-	Log       *LogConfig    `json:"log,omitempty"`
-	Inbounds  []interface{} `json:"inbounds"`
-	Outbounds []interface{} `json:"outbounds"`
+    Log       *LogConfig    `json:"log,omitempty"`
+    Inbounds  []interface{} `json:"inbounds"`
+    Outbounds []interface{} `json:"outbounds"`
 }
-
-func Generate(cfg *parser.VLESSConfig) string {
-	return GenerateWithPort(cfg, 10808)
+type LogConfig struct {
+    LogLevel string `json:"loglevel"`
 }
-
-func GenerateWithPort(cfg *parser.VLESSConfig, port int) string {
-	stream := map[string]interface{}{
-		"network":  cfg.Network,
-		"security": cfg.Security,
-	}
-
-	// Transport settings
-	switch cfg.Network {
-	case "ws":
-		wsSettings := map[string]interface{}{
-			"path": cfg.Path,
-		}
-		if cfg.HostHeader != "" {
-			wsSettings["headers"] = map[string]interface{}{
-				"Host": cfg.HostHeader,
-			}
-		}
-		stream["wsSettings"] = wsSettings
-	case "grpc":
-		grpcSettings := map[string]interface{}{
-			"serviceName": cfg.ServiceName,
-		}
-		if cfg.Mode != "" {
-			grpcSettings["mode"] = cfg.Mode
-		}
-		if cfg.Authority != "" {
-			grpcSettings["authority"] = cfg.Authority
-		}
-		stream["grpcSettings"] = grpcSettings
-	case "xhttp", "splithttp":
-		xhttpSettings := map[string]interface{}{
-			"path": cfg.Path,
-		}
-		if cfg.HostHeader != "" {
-			xhttpSettings["host"] = cfg.HostHeader
-		}
-		if cfg.Mode != "" {
-			xhttpSettings["mode"] = cfg.Mode
-		}
-		stream["xhttpSettings"] = xhttpSettings
-	}
-
-	// TLS / Reality
-	if cfg.Security == "reality" {
-		stream["realitySettings"] = map[string]interface{}{
-			"serverName":  cfg.SNI,
-			"publicKey":   cfg.PublicKey,
-			"shortId":     cfg.ShortID,
-			"fingerprint": cfg.Fingerprint,
-		}
-	} else if cfg.Security == "tls" {
-		stream["tlsSettings"] = map[string]interface{}{
-			"serverName":    cfg.SNI,
-			"allowInsecure": true,
-		}
-	}
-
-	xrayConf := XrayConfig{
-		Log: &LogConfig{LogLevel: "none"}, // отключаем все логи xray
-		Inbounds: []interface{}{
-			map[string]interface{}{
-				"port":     port,
-				"listen":   "127.0.0.1",
-				"protocol": "socks",
-				"settings": map[string]interface{}{
-					"auth": "noauth",
-				},
-			},
-		},
-		Outbounds: []interface{}{
-			map[string]interface{}{
-				"protocol": "vless",
-				"tag":      "vless-out",
-				"settings": map[string]interface{}{
-					"vnext": []interface{}{
-						map[string]interface{}{
-							"address": cfg.Host,
-							"port":    toInt(cfg.Port),
-							"users": []interface{}{
-								map[string]interface{}{
-									"id":         cfg.ID,
-									"encryption": "none",
-									"flow":       cfg.Flow,
-								},
-							},
-						},
-					},
-				},
-				"streamSettings": stream,
-			},
-		},
-	}
-
-	fileName := fmt.Sprintf("config_%d_%s.json", port, time.Now().Format("150405"))
-	f, err := os.Create(fileName)
-	if err != nil {
-		log.Fatalf("[hellcat] Error writing config file: %v", err)
-	}
-	defer f.Close()
-
-	encoder := json.NewEncoder(f)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(xrayConf); err != nil {
-		log.Fatalf("[hellcat] Error encoding config JSON: %v", err)
-	}
-
-	return fileName
+func Generate(cfg *parser.OutboundConfig) string {
+    return GenerateWithPort(cfg, 10808)
 }
-
-func toInt(s string) int {
-	var i int
-	fmt.Sscanf(s, "%d", &i)
-	return i
+func GenerateWithPort(cfg *parser.OutboundConfig, port int) string {
+    // SOCKS inbound
+    inbound := map[string]interface{}{
+        "port":     port,
+        "listen":   "127.0.0.1",
+        "protocol": "socks",
+        "settings": map[string]interface{}{"auth": "noauth"},
+    }
+    // Основной outbound
+    outbound := map[string]interface{}{
+        "protocol": cfg.Protocol,
+        "tag":      cfg.Tag,
+        "settings": cfg.Settings, // уже правильный тип
+    }
+    // streamSettings оставляем как есть (уже заполнен парсером)
+    if cfg.StreamSetting != nil {
+        stream := map[string]interface{}{}
+        stream["network"] = cfg.StreamSetting.Network
+        stream["security"] = cfg.StreamSetting.Security
+        if cfg.StreamSetting.TlsSettings != nil {
+            stream["tlsSettings"] = cfg.StreamSetting.TlsSettings
+        }
+        if cfg.StreamSetting.RealitySettings != nil {
+            stream["realitySettings"] = cfg.StreamSetting.RealitySettings
+        }
+        if cfg.StreamSetting.WsSettings != nil {
+            stream["wsSettings"] = cfg.StreamSetting.WsSettings
+        }
+        if cfg.StreamSetting.GRPCConfig != nil {
+            stream["grpcSettings"] = cfg.StreamSetting.GRPCConfig
+        }
+        if cfg.StreamSetting.XhttpSettings != nil {
+            stream["xhttpSettings"] = cfg.StreamSetting.XhttpSettings
+        }
+        outbound["streamSettings"] = stream
+    }
+    // Mux (по желанию)
+    if cfg.Mux.Enabled {
+        outbound["mux"] = map[string]interface{}{
+            "enabled":     cfg.Mux.Enabled,
+            "concurrency": cfg.Mux.Concurrency,
+        }
+    }
+    xrayConf := XrayConfig{
+        Log:       &LogConfig{LogLevel: "none"},
+        Inbounds:  []interface{}{inbound},
+        Outbounds: []interface{}{outbound},
+    }
+    fileName := fmt.Sprintf("config_%d_%s.json", port, time.Now().Format("150405"))
+    f, err := os.Create(fileName)
+    if err != nil {
+        log.Fatalf("Error writing config file: %v", err)
+    }
+    defer f.Close()
+    encoder := json.NewEncoder(f)
+    encoder.SetIndent("", "  ")
+    if err := encoder.Encode(xrayConf); err != nil {
+        log.Fatalf("Error encoding config JSON: %v", err)
+    }
+    return fileName
 }
